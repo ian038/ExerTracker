16447021e16853b13987b66049ba742d2606422c
ian038 <ianphua3@gmail.com>
2020-03-19 16:49:49 -0400

    Added api endpoints for exercise

--- server/routes/exercises.js
+++ server/routes/exercises.js
@@ -4,12 +4,16 @@ const { Exercise } = require('../db/sequelize');
 //  GET all Exercises
 router.route('/').get((req, res) => {
     Exercise.findAll()
-            .then(users => res.json(users))
+            .then(exercises => res.json(exercises))
             .catch(err => res.status(400).json('Error' + err))
 });
 
 // GET specific Exercise
-
+router.route('/:id').get((req, res) => {
+    Exercise.findOne({ where: { id: req.params.id } })
+            .then(exercise => res.json(exercise))
+            .catch(err => res.status(400).json('Error' + err))
+});
 
 //  ADD new Exercise
 router.route('/add').post((req, res) => {
@@ -36,6 +40,10 @@ router.route('/update/:id').post((req, res) => {
 });
 
 //  DELETE Exercise
-
+router.route('/:id').delete((req, res) => {
+    Exercise.findOne({ where: { id: req.params.id } })
+    .then(() => res.json('Exercise Deleted!'))
+    .catch(err => res.status(400).json('Error' + err))
+});
 
 module.exports = router;
