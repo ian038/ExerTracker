412adafea2a202bc886830d4465b4d93d9c5b68c
ian038 <ianphua3@gmail.com>
2020-03-20 17:58:58 -0400

    Added vue and vuetify helo world

---
+++ client/src/components/navbar.vue
@@ -0,0 +1,71 @@
+<template>
+    <div>
+      
+    <v-app-bar color="light-blue lighten-1">
+    <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+
+      <v-toolbar-title class="text-justify">{{msg}}</v-toolbar-title>
+      <router-link to="/foo">Go to Foo</router-link>
+      <router-link to="/bar">Go to Bar</router-link>
+    </v-app-bar>
+     <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+    >
+        <v-list-item-group
+          v-model="group"
+        >
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-account</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Account</v-list-item-title>
+          </v-list-item>
+
+        </v-list-item-group>
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+Vue.use(VueRouter);
+
+export default {
+  name: 'Navbar',
+  props: {
+    msg: String
+  },
+   data: () => ({
+      drawer: false,
+    }),
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
