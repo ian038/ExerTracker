93a08dabc81e450f87d52f8722282324de5fbc2c
ian038 <ianphua3@gmail.com>
2020-03-30 19:19:26 -0400

    Separated landing page and home page contents

--- client/src/components/ExercisesList.vue
+++ client/src/components/ExercisesList.vue
@@ -1,33 +1,43 @@
 <template>
-    <v-card class="mx-auto" width="1500px">
-        <v-card-title>Logged Exercises</v-card-title>
-        <v-simple-table>
-        <thead>
-            <tr>
-                <th>Username</th>
-                <th>Description</th>
-                <th>Duration</th>
-                <th>Date</th>
-                <th>Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr v-for="exercise in exercises" :key="exercise.id">
-                <td>{{exercise.username}}</td>
-                <td>{{exercise.description}}</td>
-                <td>{{exercise.duration}}</td>
-                <td>{{exercise.date.substring(0, 10)}}</td>
-                <td><v-btn outlined class="mx-0 mt-3 mb-3" :href="'/edit/' + exercise.id">Edit</v-btn> | <v-btn outlined color="error" class="mx-0 mt-3 mb-3" @click="Delete(exercise.id)">Delete</v-btn></td>
-            </tr>
-        </tbody>
-    </v-simple-table>
-    </v-card>
+    <v-app>
+        <Dashboard />
+        <v-container>
+        <v-card class="mx-auto" width="1500px">
+            <v-card-title>Logged Exercises</v-card-title>
+            <v-simple-table>
+            <thead>
+                <tr>
+                    <th>Username</th>
+                    <th>Description</th>
+                    <th>Duration</th>
+                    <th>Date</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="exercise in exercises" :key="exercise.id">
+                    <td>{{exercise.username}}</td>
+                    <td>{{exercise.description}}</td>
+                    <td>{{exercise.duration}}</td>
+                    <td>{{exercise.date.substring(0, 10)}}</td>
+                    <td><v-btn outlined class="mx-0 mt-3 mb-3" :href="'/edit/' + exercise.id">Edit</v-btn> | <v-btn outlined color="error" class="mx-0 mt-3 mb-3" @click="Delete(exercise.id)">Delete</v-btn></td>
+                </tr>
+            </tbody>
+            </v-simple-table>
+        </v-card>
+        </v-container>
+    </v-app>
 </template>
 
 <script>
 import axios from 'axios';
+import Dashboard from './Dashboard';
+
 
 export default {
+    components: {
+        Dashboard
+    },
     data: () => {
         return {
             exercises: []
