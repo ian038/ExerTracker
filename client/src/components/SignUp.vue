a7e9c376f3dc3243ac8cf0b3632ea9312401609e
ian038 <ianphua3@gmail.com>
2020-05-13 09:57:04 -0400

    WIP

---
+++ client/src/components/SignUp.vue
@@ -0,0 +1,74 @@
+<template>
+    <v-app>
+    <Dashboard />
+    <v-container>
+      <v-card class="mx-auto" max-width="600px">
+      <v-card-title>Sign Up</v-card-title>
+      <v-form ref="form" lazy-validation class="px-3">
+        <v-text-field v-model="username" label="Username" :rules="usernameRules"></v-text-field>
+        <v-text-field v-model="email" label="Email" :rules="emailRules"></v-text-field>
+        <v-text-field v-model="password" label="Password" :rules="passwordRules" :type="show1 ? 'text' : 'password'" ></v-text-field>
+        <v-btn outlined color="indigo" class="mx-0 mt-3 mb-3" @click="Submit" :loading="loading">Submit</v-btn>
+    </v-form>
+  </v-card>
+    </v-container>
+    </v-app>
+</template>
+
+<script>
+import axios from 'axios';
+import Dashboard from './Dashboard';
+
+
+export default {
+      components: {
+        Dashboard
+    },
+    data: () => {
+        return {
+            username:'',
+            usernameRules: [
+                v => !!v || 'Username is required'
+            ],
+            email:'',
+            emailRules: [
+                v => !!v || 'Email is required'
+            ],
+            show1: false,
+            password:'',
+            passwordRules: [
+                v => !!v || 'Password is required',
+                v => v.length >= 6 || 'Minimum 6 characters'
+            ],
+            loading: false
+        }
+    },
+    methods: {
+        Submit(e) {
+            e.preventDefault();
+            if(this.$refs.form.validate()) {
+                this.loading = true
+                const user = {
+                    username: this.username,
+                    email: this.email,
+                    password: this.password
+                }
+                console.log(user)
+                axios({
+                    method: 'post',
+                    url: 'http://localhost:5000/auth/signup',
+                    headers: {
+                        Accept: '*/*',
+                        'Content-Type': 'application/json'
+                    },
+                    data: {
+                        user
+                    }
+                })
+                .then(res => console.log(res.data))
+                .catch(err => console.log(err))
+            }
+        }
+    }
+}
+</script>
