b1b07cb8327e7f8d422b3242372b861ec235030f
Ian Phua <ianphua3@gmail.com>
2020-05-17 17:25:32 +0000

    Update router.js

--- client/src/router.js
+++ client/src/router.js
@@ -1,7 +1,6 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 
-import Landing from './components/Landing.vue';
 import CreateExercises from './components/CreateExercises.vue';
 import CreateUsers from './components/CreateUsers.vue';
 import EditExercises from './components/EditExercises.vue';
@@ -10,8 +9,7 @@ import ExercisesList from './components/ExercisesList.vue';
 Vue.use(VueRouter);
 
 const routes = [
-    { path: '/', component: Landing },
-    { path: '/home', component: ExercisesList },
+    { path: '/', component: ExercisesList },
     { path: '/edit/:id', component: EditExercises },
     { path: '/create', component: CreateExercises },
     { path: '/user', component: CreateUsers },
