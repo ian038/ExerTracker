ba3e65107299ee9f52889d391e3320cd2cac3cd1
ian038 <ianphua3@gmail.com>
2020-05-18 15:17:57 -0400

    added static folder and prepared SPA

---
+++ server/server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(cookieParser());
+
+// Connect to routes
+const exerciseRouter = require('./routes/exercises');
+const userRouter = require('./routes/users');
+const authRouter = require('./routes/auth')
+
+app.use('/exercise', exerciseRouter);
+app.use('/user', userRouter);
+app.use('/auth', authRouter);
+
+
+const port = process.env.PORT || 5000;
+
+// Handle production 
+if(process.env.NODE_ENV === 'production') {
+    // static folder
+    app.use(express.static(__dirname + '/public/'))
+    // handle SPA
+    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'))
+}
+
+app.listen(port, () => {
+    console.log('running on ' + port);
+})
