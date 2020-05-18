fe4bb5104098d6832e6ec859df92c6a7f6c17b7b
ian038 <ianphua3@gmail.com>
2020-05-18 16:25:54 -0400

    fixing bugs

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ if(process.env.NODE_ENV === 'production') {
     // static folder
     app.use(express.static(__dirname + '/public/'))
     // handle SPA
-    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'))
+    app.get('*', (req, res) => res.sendFile(__dirname + '/public/index.html'))
 }
 
 app.listen(port, () => {
