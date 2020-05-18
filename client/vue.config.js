a33cd60bb533a2a07831789b1b0798a169e6d2c7
ian038 <ianphua3@gmail.com>
2020-05-18 15:44:49 -0400

    Fixed bugs

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,6 +1,19 @@
 const path = require('path')
 
 module.exports = {
+  devServer: {
+    proxy: {
+      '/exercise': {
+        target: 'http://localhost:5000'
+      },
+      '/user': {
+        target: 'http://localhost:5000'
+      },
+      '/auth': {
+        target: 'http://localhost:5000'
+      }
+    }
+  },
   outputDir: path.resolve(__dirname, '../server/public'),
   "transpileDependencies": [
     "vuetify"
